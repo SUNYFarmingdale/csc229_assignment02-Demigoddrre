9cd4ea558b82235cce0a4c93010df44fb3d638dc
dandr <dandr@DESKTOP-D985USU>
2023-02-10 16:29:38 -0500

    Completed assignment.

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -1,5 +1,7 @@
 package com.mycompany.csc229_211review_lab02hw;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MoaathAlrajab
@@ -12,12 +14,17 @@ public class DriverClass {
 		// ToDo 6: Fix the constructor of Student class
 		
 		// ToDo 7: Add a toString method for Student class
-		
+                
+                
+		//changed short to int for age var
 		Student std1= new Student("James", 20);
 		
 		// ToDo 8: Set the gpa of the student using the scanner and user
 		// 			input and then print the output.
-		
+		Scanner input = new Scanner(System.in);
+                System.out.print("Enter the student's GPA: ");
+                double gpa = input.nextDouble();
+                std1.setGpa(gpa);
 		System.out.println(std1);
 		
 		// ToDo 9: add comments and explain your code
